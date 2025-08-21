c90d373910a0298ef5e6ac3f0b266b508c4eae00
Beytullah Atasoy <beytullahatasoy55@gmail.com>
2025-08-22 00:17:29 +0300

    Add files via upload

--- python/Main.py
+++ python/Main.py
@@ -35,10 +35,10 @@ data = np.array(data)
 train_size = int(0.8 * len(data))   # %80 eğitim, %20 test
 
 
-X_train = torch.from_numpy(data[:train_size, :-1, :])
-y_train = torch.from_numpy(data[:train_size, -1, :])
-X_test  = torch.from_numpy(data[train_size:, :-1, :])
-y_test  = torch.from_numpy(data[train_size:, -1, :])
+X_train = torch.from_numpy(data[:train_size, :-1, :]).type(torch.Tensor).to(device)
+y_train = torch.from_numpy(data[:train_size, -1, :]).type(torch.Tensor).to(device)
+X_test  = torch.from_numpy(data[train_size:, :-1, :]).type(torch.Tensor).to(device)
+y_test  = torch.from_numpy(data[train_size:, -1, :]).type(torch.Tensor).to(device)
 
 class PredictionModel(nn.Module):
     def __init__(self, input_dim, hidden_dim, num_layers, output_dim):
@@ -51,3 +51,72 @@ class PredictionModel(nn.Module):
         self.fc   = nn.Linear(hidden_dim, output_dim)
 
     def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim, device=device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim, device=device)
+
+        out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
+        out = self.fc(out[:, -1, :])
+
+        return out
+
+model = PredictionModel(input_dim=1, hidden_dim=32, num_layers=2, output_dim=1).to(device)
+
+criterion = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=0.01)
+
+
+num_epochs = 200
+
+for i in range(num_epochs):
+    y_train_pred = model(X_train)
+    
+    loss = criterion(y_train_pred, y_train)
+
+    if i % 25 == 0:
+        print(i, loss.item())
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+
+model.eval()
+
+y_test_pred = model(X_test)
+
+y_train_pred = scaler.inverse_transform(y_train_pred.detach().cpu().numpy())
+y_train      = scaler.inverse_transform(y_train.detach().cpu().numpy())
+y_test_pred  = scaler.inverse_transform(y_test_pred.detach().cpu().numpy())
+y_test       = scaler.inverse_transform(y_test.detach().cpu().numpy())
+
+train_rmse = root_mean_squared_error(y_train[:, 0], y_train_pred[:, 0])
+test_rmse  = root_mean_squared_error(y_test[:, 0], y_test_pred[:, 0])
+
+
+train_rmse
+
+test_rmse
+
+
+fig = plt.figure(figsize=(12, 10))
+
+gs = fig.add_gridspec(4, 1)
+
+ax1 = fig.add_subplot(gs[:3, 0])
+ax1.plot(df.iloc[-len(y_test):].index, y_test, color='blue', label='Actual Price')
+ax1.plot(df.iloc[-len(y_test):].index, y_test_pred, color='green', label='Predicted Price')
+ax1.legend()
+plt.title(f"{ticker} Stock Price Prediction")
+plt.xlabel('Date')
+plt.ylabel('Price')
+
+ax2 = fig.add_subplot(gs[3, 0])
+ax2.axhline(test_rmse, color='blue', linestyle='--', label='RMSE')
+ax2.plot(df.iloc[-len(y_test):].index, abs(y_test - y_test_pred), 'r', label='Prediction Error')
+ax2.legend()
+plt.title("Prediction Error")
+plt.xlabel('Date')
+plt.ylabel('Error')
+
+plt.tight_layout()
+plt.show()
